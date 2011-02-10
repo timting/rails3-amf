4760304c33b22acd7d304c997ba202133efec547
Lyle Underwood <lyleunderwood@gmail.com>
2011-02-10 15:20:56 -0800

    Don't send model instances to actions

--- lib/rails3-amf/request_processor.rb
+++ lib/rails3-amf/request_processor.rb
@@ -72,6 +72,11 @@ module Rails3AMF
       params = {}
       args.each_with_index {|obj, i| params[i] = obj}
       params.merge!(@config.mapped_params(controller_name, method_name, args))
+
+      params = params.merge(params) do |a, v|
+        v = v.attributes if v.respond_to? :attributes
+      end
+
       params
     end
   end
